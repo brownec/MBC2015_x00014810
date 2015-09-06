4da3cace1cc8929de5a77e8445aa37f959988b2f
brownec <cbrowne77@gmail.com>
2015-09-06 20:34:07 +0100

    Create new project to commit to Github

---
+++ MBC2015/MBC2015/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MBC2015.MvcApplication" Language="C#" %>
---
+++ MBC2015/MBC2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MBC2015.Startup))]
+namespace MBC2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
